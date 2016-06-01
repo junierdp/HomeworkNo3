859a7e9030efe688f3951792a48109f41af85792
Junier Damian <onieldp@gmail.com>
2016-06-01 01:11:46 -0400

    Agregando el archivo config.php que contiene los parametros necesarios para establecer la conexion a la base de datos.

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	//Definiendo los parametros para hacer la conexion a la BD.
+	define('DB_HOST','localhost:3307');  //define: define constante y valores
+	define('DB_NAME','registrop'); 
+	define('DB_USER','root'); 
+	define('DB_PASS','oniel2908'); 
+?>
